8723852dd36e4c9d24b1a0fdf7b68e6c100186db
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-04-28 11:34:14 +0530

    Implemented Full Screen Directive

--- angular-15-image-viewer/projects/angular-image-viewer/src/lib/directive/full-screen.directive.ts
+++ angular-15-image-viewer/projects/angular-image-viewer/src/lib/directive/full-screen.directive.ts
@@ -1,10 +1,45 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Directive({
-  selector: '[libFullScreen]'
+  selector: '[appFullScreen]'
 })
-export class FullScreenDirective {
+export class FullScreenDirective implements OnChanges {
+  @Input("appScreenfull") fullscreenState!: boolean;
+  constructor(private el: ElementRef) { }
 
-  constructor() { }
+  ngOnChanges(changes: SimpleChanges) {
+    if (!changes["fullscreenState"].isFirstChange()) {
+      if (this.fullscreenState) {
+        const element: any = this.el.nativeElement;
+        const requestMethod =
+          element.requestFullscreen ||
+          element.webkitRequestFullScreen ||
+          element.mozRequestFullScreen ||
+          element.msRequestFullScreen;
+
+        if (requestMethod) {
+          // Native full screen.
+          requestMethod.call(element);
+        } else {
+          return null;
+        }
+      } else {
+        const element: any = document;
+        const requestMethod =
+          element.cancelFullscreen ||
+          element.webkitExitFullscreen ||
+          element.webkitCancelFullScreen ||
+          element.mozCancelFullScreen ||
+          element.msExitFullScreen;
+
+        if (requestMethod) {
+          // Native Cancel full screen.
+          requestMethod.call(element);
+        } else {
+          return null;
+        }
+      }
+    }
+  }
 
 }
