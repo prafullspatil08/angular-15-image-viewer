847e521429ef518ccafb3089a2ff11063b028162
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-04-28 11:10:34 +0530

    Generated Library

---
+++ angular-15-image-viewer/projects/angular-image-viewer/src/lib/angular-image-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-image-viewer',
+  template: `
+    <p>
+      angular-image-viewer works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AngularImageViewerComponent {
+
+}
---
+++ angular-15-image-viewer/projects/angular-image-viewer/src/lib/angular-image-viewer.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { AngularImageViewerComponent } from './angular-image-viewer.component';
+
+
+
+@NgModule({
+  declarations: [
+    AngularImageViewerComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    AngularImageViewerComponent
+  ]
+})
+export class AngularImageViewerModule { }
